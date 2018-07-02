64ac9d37bed6ffd81cfcba7fe18575b8f236bf3f
whming <122627018@qq.com>
2018-07-02 21:10:54 +0800

    图的深度遍历，广度遍历的应用

--- src/graph/graphoption.go
+++ src/graph/graphoption.go
@@ -1,8 +1,13 @@
 package graph
 
-var thisData = []string{"a","b","c","d","e","f"}
+import (
+	"fmt"
+	"strconv"
+)
 
-func CreateMGraph() (*MGraph) {
+var thisData = []string{"a", "b", "c", "d", "e", "f"}
+
+func CreateMGraph() *MGraph {
 	var mGrgph *MGraph
 	mGrgph = &MGraph{}
 	i := 0
@@ -22,8 +27,11 @@ func CreateMGraph() (*MGraph) {
 	mGrgph.edges[2][3] = 1
 	mGrgph.edges[3][4] = 1
 	mGrgph.edges[3][5] = 1
+	mGrgph.edges[0][4] = 1
+	mGrgph.edges[4][3] = 1
+	mGrgph.edges[0][2] = 1
 	mGrgph.n = 6
-	mGrgph.e = 6
+	mGrgph.e = 9
 	return mGrgph
 }
 
@@ -66,7 +74,7 @@ func MatToList(mGraph *MGraph, alGraph *ALGraph) {
 	}
 }
 
-func CreateALGraph() (*ALGraph) {
+func CreateALGraph() *ALGraph {
 	var alGraph *ALGraph
 	i := 0
 	for {
@@ -95,7 +103,6 @@ var Viste [MAXV]int
  * 深度优先遍历
  */
 func DFS(alGraph *ALGraph, v int) {
-	//log.Print("DFS->v:"+strconv.Itoa(v))
 	vNode := alGraph.adjlist[v]
 	print(vNode.data.data)
 	Viste[v] = 1
@@ -109,4 +116,241 @@ func DFS(alGraph *ALGraph, v int) {
 		}
 		arcNode = arcNode.nextarc
 	}
+}
+
+//广度优先遍历
+func BFS(alGraph *ALGraph, v int) {
+	queue := make([]int,MAXV)
+	front := -1
+	rear := -1
+	rear++
+	queue[rear] = v
+	Viste[v] = 1
+	fmt.Print(alGraph.adjlist[v].data.data)
+	for {
+		if rear == front {
+			break
+		}
+		front++
+		tmp := queue[front]
+		vNode := alGraph.adjlist[tmp]
+		nextNode := vNode.firstarc
+		for {
+			if nextNode == nil {
+				break
+			}
+			if Viste[nextNode.adjvex] == 0 {
+				rear++
+				queue[rear] = nextNode.adjvex
+				fmt.Print(alGraph.adjlist[nextNode.adjvex].data.data)
+				Viste[nextNode.adjvex] = 1
+			}
+			nextNode = nextNode.nextarc
+		}
+	}
+	fmt.Println("")
+}
+
+//假设图G采用邻接表存储，设计一个算法输出图G中从顶点u到v的一条简单路径
+//d的作用很大 想清楚
+func FindPath1(alGraph *ALGraph, u, v, d int, path []int) {
+	//fmt.Println("目前检查的点："+strconv.Itoa(u))
+	vNode := alGraph.adjlist[u]
+	d++
+	path[d] = u
+	Viste[u] = 1
+	if u == v {
+		tmp := 0
+		for {
+			if tmp > len(path)-1 {
+				break
+			}
+			if path[tmp] != -1 {
+				fmt.Println(alGraph.adjlist[path[tmp]].data.data)
+				if path[tmp] == v {
+					return
+				}
+			}
+			tmp++
+		}
+
+	}
+	arcNode := vNode.firstarc
+	for {
+		if arcNode == nil {
+			break
+		}
+		if Viste[arcNode.adjvex] == 0 {
+			FindPath1(alGraph, arcNode.adjvex, v, d, path)
+		}
+		arcNode = arcNode.nextarc
+	}
+}
+
+//假设图G采用邻接表存储，设计一个算法输出图G中从顶点u到v的所有简单路径
+func FindPath2(alGraph *ALGraph, u, v, d int, path []int) {
+	fmt.Println("目前检查的点：" + strconv.Itoa(u))
+	vNode := alGraph.adjlist[u]
+	d++
+	path[d] = u
+	Viste[u] = 1
+	if u == v {
+		tmp := 0
+		for {
+			if tmp > len(path)-1 {
+				break
+			}
+			if path[tmp] != -1 {
+				//
+				fmt.Print(alGraph.adjlist[path[tmp]].data.data)
+				if path[tmp] == v {
+					break
+				}
+			}
+			tmp++
+		}
+		fmt.Println("")
+	}
+	arcNode := vNode.firstarc
+	for {
+		if arcNode == nil {
+			break
+		}
+		if Viste[arcNode.adjvex] == 0 {
+			FindPath2(alGraph, arcNode.adjvex, v, d, path)
+		}
+		Viste[arcNode.adjvex] = 0
+		arcNode = arcNode.nextarc
+
+	}
+}
+
+//假设图G采用邻接表存储，设计一个算法输出图G中从顶点u到v的所有长度为L的所有简单路径
+func FindPath3(alGraph *ALGraph, u, v, d, l int, path []int) {
+	vNode := alGraph.adjlist[u]
+	d++
+	path[d] = u
+	Viste[u] = 1
+	if u == v && d == l {
+		tmp := 0
+		for {
+			if tmp > len(path)-1 {
+				break
+			}
+			if path[tmp] != -1 {
+				//
+				fmt.Print(alGraph.adjlist[path[tmp]].data.data)
+				if path[tmp] == v {
+					break
+				}
+			}
+			tmp++
+		}
+		fmt.Println("")
+	}
+	arcNode := vNode.firstarc
+	for {
+		if arcNode == nil {
+			break
+		}
+		if Viste[arcNode.adjvex] == 0 {
+			FindPath3(alGraph, arcNode.adjvex, v, d, l, path)
+		}
+		Viste[arcNode.adjvex] = 0
+		arcNode = arcNode.nextarc
+
+	}
+}
+
+//假设图G采用邻接表存储，设计一个算法求图中通过某顶点k的所有简单回路
+func FindPath4(alGraph *ALGraph, u, v, d int, path []int) {
+	//fmt.Println("目前检查的点：" + strconv.Itoa(u))
+	d++
+	path[d] = u
+	Viste[u] = 1
+	vNode := alGraph.adjlist[u]
+	nextNode := vNode.firstarc
+	for {
+		if nextNode == nil {
+			break
+		}
+		//打印
+		if nextNode.adjvex == v && d > 0 {
+			tmp := 0
+			for {
+				if tmp > len(path)-1 {
+					break
+				}
+				if path[tmp] != -1 {
+					fmt.Print(alGraph.adjlist[path[tmp]].data.data)
+				}
+				tmp++
+			}
+			fmt.Print(alGraph.adjlist[v].data.data)
+			fmt.Println("")
+		}
+
+		if Viste[nextNode.adjvex] == 0 {
+			FindPath4(alGraph, nextNode.adjvex, v, d, path)
+		}
+		nextNode = nextNode.nextarc
+	}
+	//fmt.Println("回滚：" + strconv.Itoa(u))
+	path[d] = -1
+	Viste[u] = 0
+}
+
+//假设图G采用邻接表存储，设计一个算法求不带权无向连通图G中从定点u到定点v的一条最短路径
+func ShortPath(alGraph *ALGraph, u, v int){
+	type QUERE struct {
+		data int
+		parent int
+	}
+	rear := -1
+	front := -1
+	qu := make([]QUERE,alGraph.e)
+	Viste[u] = 1
+	rear ++
+	qu[rear] = QUERE{
+		data : u,
+		parent : -1,
+	}
+
+	for {
+		if rear == front {
+			break
+		}
+		front ++
+		quItem := qu[front]
+		if quItem.data == v {
+			i := front
+			for{
+				if qu[i].parent == -1 {
+					break
+				}
+				fmt.Print(alGraph.adjlist[qu[i].data].data.data)
+				i = qu[i].parent
+			}
+			fmt.Print(alGraph.adjlist[qu[i].data].data.data)
+			break
+		}
+		arcNode := alGraph.adjlist[quItem.data].firstarc
+
+		for{
+			if arcNode == nil {
+				break
+			}
+			if Viste[arcNode.adjvex] == 0{
+				rear ++
+				Viste[arcNode.adjvex] = 1
+				qu[rear] = QUERE{
+					data : arcNode.adjvex,
+					parent : front,
+				}
+			}
+			arcNode = arcNode.nextarc
+		}
+
+	}
+
 }
--- src/graph/option_test.go
+++ src/graph/option_test.go
@@ -8,3 +8,102 @@ func TestDFS(t *testing.T) {
 	MatToList(m,alGraph)
 	DFS(alGraph,1)
 }
+
+func TestBFS(t *testing.T) {
+	var alGraph = &ALGraph{}
+	m := CreateMGraph()
+	MatToList(m,alGraph)
+	BFS(alGraph,0)
+}
+
+//假设图G采用邻接表存储，设计一个算法输出图G中从顶点u到v的一条简单路径
+func TestFindPath1(t *testing.T) {
+	var alGraph = &ALGraph{}
+	m := CreateMGraph()
+	MatToList(m,alGraph)
+	//把头结点的俩个结点的位置倒换一下
+	//vNode := alGraph.adjlist[0].firstarc
+	//alGraph.adjlist[0].firstarc = vNode.nextarc
+	//alGraph.adjlist[0].firstarc.nextarc = vNode
+	//vNode.nextarc = nil
+
+	path := make([]int,alGraph.e)
+	tmp := 0
+	for{
+		if tmp > alGraph.e -1 {
+			break
+		}
+		path[tmp] = -1
+		tmp++
+	}
+	FindPath1(alGraph,0,5,0, path)
+}
+
+//假设图G采用邻接表存储，设计一个算法输出图G中从顶点u到v的所有简单路径
+func TestFindPath2(t *testing.T) {
+	var alGraph = &ALGraph{}
+	m := CreateMGraph()
+	MatToList(m,alGraph)
+	path := make([]int,alGraph.e)
+	tmp := 0
+	for{
+		if tmp > alGraph.e -1 {
+			break
+		}
+		path[tmp] = -1
+		tmp++
+	}
+	FindPath2(alGraph,0,5,0, path)
+}
+
+//假设图G采用邻接表存储，设计一个算法输出图G中从顶点u到v的所有长度为L的所有简单路径
+func TestFindPath3(t *testing.T) {
+	var alGraph = &ALGraph{}
+	m := CreateMGraph()
+	MatToList(m,alGraph)
+	path := make([]int,alGraph.e)
+	tmp := 0
+	for{
+		if tmp > alGraph.e -1 {
+			break
+		}
+		path[tmp] = -1
+		tmp++
+	}
+	FindPath3(alGraph,0,5,0, 4, path)
+}
+
+
+//假设图G采用邻接表存储，设计一个算法求图中通过某顶点k的所有简单回路
+func TestFindPath4(t *testing.T) {
+	var alGraph = &ALGraph{}
+	m := CreateMGraph()
+	MatToList(m,alGraph)
+	path := make([]int,alGraph.e)
+	tmp := 0
+	for{
+		if tmp > alGraph.e -1 {
+			break
+		}
+		path[tmp] = -1
+		tmp++
+	}
+	FindPath4(alGraph,2,2,-1, path)
+}
+
+func TestShortPath(t *testing.T) {
+	var alGraph = &ALGraph{}
+	m := CreateMGraph()
+	MatToList(m,alGraph)
+	path := make([]int,alGraph.e)
+	tmp := 0
+	for{
+		if tmp > alGraph.e -1 {
+			break
+		}
+		path[tmp] = -1
+		tmp++
+	}
+	ShortPath(alGraph,0,5)
+}
+
